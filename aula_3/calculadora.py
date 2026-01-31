ee0b7b15badb475c2d86e8d4553da5515514a10a
Cauan <ssantanacauan@gmail.com>
2026-01-31 14:33:01 -0300

    Add files via upload

---
+++ aula_3/calculadora.py
@@ -0,0 +1,20 @@
+print('Calculadora Python')
+print('...' * 8 )
+
+n1 = int(input('digite um numero: '))
+n2 = int(input('digite outro numero: '))
+print()
+
+
+soma = n1 + n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+subtracao = n1 - n2
+
+print(n1, '+', n2, '=', soma)
+print()
+print(n1, '/', n2, '=', divisao)
+print()
+print(n1, '*', n2, '=', multiplicacao)
+print()
+print(n1, '-', n2, '=', subtracao)
